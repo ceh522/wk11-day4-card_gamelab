6dfccdd2d94ce03f25a1838cb56e1495df6e75c6
Catherine Hall <catherine1992123@hotmail.com>
2020-02-13 15:07:20 +0000

    Completed mvp

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Deck {
+
+    private ArrayList<Card> cards;
+
+    public Deck() {
+        this.cards = createDeck();
+    }
+
+    public static ArrayList<Card> createDeck() {
+        ArrayList<Card> cards = new ArrayList<Card>();
+
+        for (Suit suit : Suit.values()) {
+            for (Type type : Type.values()) {
+                cards.add(new Card(suit, type));
+            }
+        }
+
+        return cards;
+    }
+
+    public Card dealFirstCard() {
+        return this.cards.get(0);
+    }
+
+    public int getCardArrayLength() {
+        return this.cards.size();
+    }
+
+    public void shuffleDeck() {
+       Collections.shuffle(this.cards);
+    }
 }
+
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeckTest {
+
+    Deck deck;
+
+    @Before
+    public void before(){
+        deck = new Deck ();
+    }
+
+    @Test
+    public void checkDeckHas52Cards(){
+        assertEquals(52, deck.getCardArrayLength());
+        assertEquals(true, deck.dealFirstCard() instanceof Card);
+    }
+
 }
